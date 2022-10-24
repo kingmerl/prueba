0f1a066af1dad2f9443ecbc4e621a4f5a5ce4b08
Marlon <marlon@ramlop.com>
2022-10-24 16:23:06 -0600

    asdfxxxx

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def load_user(id):
     return ModelUser.get_by_id(db,id)
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    return 'hola mundo'
 
 @app.route('/login', methods=['GET','POST'])
 

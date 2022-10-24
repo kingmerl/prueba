ad4de0b119461db264d4ba9a47f95e85c3ddfe47
Marlon <marlon@ramlop.com>
2022-10-24 12:25:37 -0600

    regresando

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     SECRET_KEY = 'B!1weNAt1T^%kvhUI*S^'
 class DevelopmentConfig(Config):
     DEBUG=True
-    MYSQL_HOST ='b0gwjscmkvpdkeu9pfx-mysql.services.clever-cloud.com'
+    MYSQL_HOST ='b10gwjscmkvpdkeu9pfx-mysql.services.clever-cloud.com'
     MYSQL_USER = 'ukcxcpmdhcyncprw'
     MYSQL_PASSWORD = 'QI0IElFeN0PCtnQGUwgP'
     MYSQL_DB = 'b10gwjscmkvpdkeu9pfx'

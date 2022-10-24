51de5c890029622c754f2004c84f52be960ef4b2
Marlon <marlon@ramlop.com>
2022-10-23 22:35:38 -0600

    ahora config se modifico

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ class Config:
     SECRET_KEY = 'B!1weNAt1T^%kvhUI*S^'
 class DevelopmentConfig(Config):
     DEBUG=True
-    MYSQL_HOST ='localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = '1107p1'
+    MYSQL_HOST ='b10gwjscmkvpdkeu9pfx-mysql.services.clever-cloud.com'
+    MYSQL_USER = 'ukcxcpmdhcyncprw'
+    MYSQL_PASSWORD = 'QI0IElFeN0PCtnQGUwgP'
+    MYSQL_DB = 'b10gwjscmkvpdkeu9pfx'
     
 config={
     'development': DevelopmentConfig

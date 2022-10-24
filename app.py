1114f9630606c06c1dd5e541e4c1b81060763d48
Marlon <marlon@ramlop.com>
2022-10-24 11:47:28 -0600

    retorno a las a modificaciones

--- app.py
+++ app.py
@@ -31,11 +31,7 @@ def load_user(id):
     return ModelUser.get_by_id(db,id)
 @app.route('/')
 def index():
-    return redirect(url_for('/pru'))
-
-app.route('/pru')
-def pru():
-    return render_template('hola.html')
+    return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET','POST'])
 def login():

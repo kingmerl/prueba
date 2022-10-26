28032392575108de5ae8e0ded8b18e17135935f2
Marlon <marlon@ramlop.com>
2022-10-25 19:04:18 -0600

    asdfsd

--- config.py
+++ config.py
@@ -2,11 +2,6 @@
 class Config:
     SECRET_KEY = 'B!1weNAt1T^%kvhUI*S^'
 class DevelopmentConfig(Config):
-    #DEBUG=True
-    #MYSQL_HOST ='localhost'
-    #MYSQL_USER = 'root'
-    #MYSQL_PASSWORD = ''
-    #MYSQL_DB = '1107p1'
     DEBUG=True
     MYSQL_HOST ='b10gwjscmkvpdkeu9pfx-mysql.services.clever-cloud.com'
     MYSQL_USER = 'ukcxcpmdhcyncprw'
